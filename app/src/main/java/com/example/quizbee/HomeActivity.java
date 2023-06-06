5b1446bae94c219883763d8d83363232af027ef7
roopa <roopa.peddinti@improve10x.com>
2023-06-07 00:22:42 +0530

    In Themes files changed the action bar component.

--- app/src/main/java/com/example/quizbee/HomeActivity.java
+++ app/src/main/java/com/example/quizbee/HomeActivity.java
@@ -2,13 +2,29 @@ package com.example.quizbee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.quizbee.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("Home");
+        buttonClicked();
+    }
+
+    public void buttonClicked() {
+        binding.startBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, QuestionsActivity.class);
+            startActivity(intent);
+        });
     }
 }
