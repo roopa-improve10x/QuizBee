c21bd9bb0e523e141682027ad6ee610245ca6dd8
roopa <roopa.peddinti@improve10x.com>
2023-06-09 13:23:41 +0530

    In second screen design alignment is fixed.

--- app/src/main/java/com/example/quizbee/ResultActivity.java
+++ app/src/main/java/com/example/quizbee/ResultActivity.java
@@ -3,12 +3,19 @@ package com.example.quizbee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.quizbee.databinding.ActivityResultBinding;
 
 public class ResultActivity extends AppCompatActivity {
+
+    ActivityResultBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+        binding = ActivityResultBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("Result");
     }
 }
